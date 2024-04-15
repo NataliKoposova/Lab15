b984c9f9f83a4ff84bac65d7709dae2801f2c84f
Koposova1 <Koposova1@KOPOSOVANA>
2024-04-15 13:35:36 +0300

    Добавьте файлы проекта.

---
+++ Lab15/ISeries.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{   
+        interface ISeries
+        {
+            void setStart(int x);
+            int getNext();
+            void reset();
+        }
+        class ArithProgression : ISeries
+        {
+            int startX;
+            int current;
+            public int Step { get; set; }
+            public int getNext()
+            {
+                current += Step;
+                return current;
+            }
+
+            public void reset()
+            {
+                current = startX;
+            }
+
+            public void setStart(int x)
+            {
+                startX = x;
+                current = startX;
+            }
+        }
+        class GeomProgression : ISeries
+        {
+            int startX;
+            int current;
+
+            public int Step { get; set; }
+            public int getNext()
+            {
+                current *= Step;
+                return current;
+            }
+
+            public void reset()
+            {
+                current = startX;
+            }
+
+            public void setStart(int x)
+            {
+                startX = x;
+                current = startX;
+            }
+        }    
+}
---
+++ Lab15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arithProgression = new ArithProgression();
+            GeomProgression geomProgression = new GeomProgression();
+            arithProgression.setStart(3);
+            arithProgression.Step = 3;
+            Console.WriteLine("Арифметическая прогрессия");
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            arithProgression.reset();
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine(arithProgression.getNext());
+            Console.WriteLine("Геометрическая прогрессия");
+            geomProgression.setStart(3);
+            geomProgression.Step = 3;
+            Console.WriteLine(geomProgression.getNext());
+            Console.WriteLine(geomProgression.getNext());
+            geomProgression.reset();
+            Console.WriteLine(geomProgression.getNext());
+        }
+    }
+}
